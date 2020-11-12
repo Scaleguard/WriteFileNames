bb7e5371c5526957d63c05e692853b5cd74e6a95
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:24:55 +0200

    YES YES YES

---
+++ DirectorsAndFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DirectorsAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootpath = @"C:\Users\opilane\Samples";
+
+            //get the full path of all the direcroies at the rootPath
+            string[] allDirectories = Directory.GetDirectories(rootpath);
+            //ReadArray(allDirectories);
+
+            //get nested directories
+            string[] includeNestedDirectories = Directory.GetDirectories(rootpath, "*", SearchOption.AllDirectories);
+            ReadArray(includeNestedDirectories);
+
+            //get directory files
+            string[] files = Directory.GetFiles(rootpath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach (string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        } 
+    }
+}
